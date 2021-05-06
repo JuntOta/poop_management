9ab377776ee6ef0954a16626c8599af60414ccfe
JuntOta <junto.ota@ezweb.ne.jp>
2021-05-06 19:41:01 +0900

    ビューファイル設置&ユーザー管理機能導入

---
+++ app/controllers/poops_controller.rb
@@ -0,0 +1,4 @@
+class PoopsController < ApplicationController
+  def index
+  end
+end
---
+++ test/controllers/poops_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PoopsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
