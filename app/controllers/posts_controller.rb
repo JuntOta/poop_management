3da005ed979128bfa5f92854a63a078fae3e848c
JuntOta <junto.ota@ezweb.ne.jp>
2021-05-09 13:08:46 +0900

    新規相談投稿のリダイレクト実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
   def show
