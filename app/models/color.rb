940b0945382cd2ddc58db8f362cff9caf8fb0cb4
JuntOta <junto.ota@ezweb.ne.jp>
2021-05-10 20:54:03 +0900

    アクティブハッシュのname変更

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,15 +1,11 @@
 class Color < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '経済' },
-    { id: 3, name: '政治' },
-    { id: 4, name: '地域' },
-    { id: 5, name: '国際' },
-    { id: 6, name: 'IT' },
-    { id: 7, name: 'エンタメ' },
-    { id: 8, name: 'スポーツ' },
-    { id: 9, name: 'グルメ' },
-    { id: 10, name: 'その他' }
+    { id: 2, name: '黄色' },
+    { id: 3, name: '黄土色' },
+    { id: 4, name: '茶色' },
+    { id: 5, name: 'こげ茶' },
+    { id: 6, name: '黒' },
   ]
 
   include ActiveHash::Associations
