5269cd1cb7b41d756ceaa2367ac97eadae872fe2
JuntOta <junto.ota@ezweb.ne.jp>
2021-05-13 18:06:48 +0900

    相談投稿削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,12 @@ class PostsController < ApplicationController
     @comments = @post.comments.includes(:user)
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
