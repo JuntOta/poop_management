ba544cae6467c9f218daea22458aef50a6196277
JuntOta <junto.ota@ezweb.ne.jp>
2021-06-15 22:04:28 +0900

    バリデーション表示編集

--- app/javascript/show.js
+++ app/javascript/show.js
@@ -2,3 +2,9 @@
 // window.addEventListener('popstate', (e) => {
 //   history.go(1);
 // });
+
+// window.addEventListener("pageshow", function(event){
+//   if (event.persisted) { // PageTransitionEvent.persistedでキャッシュから読み込んだか分岐する
+//     window.location.reload(); //リロードする
+//   }
+// });
