165062ea1f50d3d41d51f8b66178db21f60210a1
JuntOta <junto.ota@ezweb.ne.jp>
2021-06-16 16:19:28 +0900

    量id変更

--- app/models/amount.rb
+++ app/models/amount.rb
@@ -1,11 +1,10 @@
 class Amount < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: 'メガ盛り' },
-    { id: 3, name: '大盛り' },
-    { id: 4, name: '並盛り' },
-    { id: 5, name: '小盛り' },
-    { id: 6, name: 'ちょっぴり' },
+    { id: 1, name: 'メガ盛り' },
+    { id: 2, name: '大盛り' },
+    { id: 3, name: '並盛り' },
+    { id: 4, name: '小盛り' },
+    { id: 5, name: 'ちょっぴり' },
   ]
 
   include ActiveHash::Associations
