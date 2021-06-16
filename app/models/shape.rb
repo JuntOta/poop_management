2711f0e87c59b6cb5aae67bd77a0d24f07f99113
JuntOta <junto.ota@ezweb.ne.jp>
2021-06-16 16:16:58 +0900

    かたちid変更

--- app/models/shape.rb
+++ app/models/shape.rb
@@ -1,11 +1,10 @@
 class Shape < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: 'コロコロ' },
-    { id: 3, name: 'カチカチ' },
-    { id: 4, name: 'バナナ' },
-    { id: 5, name: '半練り' },
-    { id: 6, name: 'ドロドロ' },
+    { id: 1, name: 'コロコロ' },
+    { id: 2, name: 'カチカチ' },
+    { id: 3, name: 'バナナ' },
+    { id: 4, name: '半練り' },
+    { id: 5, name: 'ドロドロ' },
   ]
 
   include ActiveHash::Associations
