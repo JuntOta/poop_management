9d9eb238d9a7e21e88a66b6e43fe4b55858576d7
JuntOta <junto.ota@ezweb.ne.jp>
2021-06-16 16:17:46 +0900

    色id変更

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,11 +1,10 @@
 class Color < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: '黄色' },
-    { id: 3, name: '黄土色' },
-    { id: 4, name: '茶色' },
-    { id: 5, name: 'こげ茶' },
-    { id: 6, name: '黒' },
+    { id: 1, name: '黄色' },
+    { id: 2, name: '黄土色' },
+    { id: 3, name: '茶色' },
+    { id: 4, name: 'こげ茶' },
+    { id: 5, name: '黒' },
   ]
 
   include ActiveHash::Associations
