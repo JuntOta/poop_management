be569a9ccd85f2f6483e523c46c099c06635e974
JuntOta <junto.ota@ezweb.ne.jp>
2021-06-16 16:20:45 +0900

    においid変更

--- app/models/smell.rb
+++ app/models/smell.rb
@@ -1,9 +1,8 @@
 class Smell < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: 'くさくない' },
-    { id: 3, name: 'くさい' },
-    { id: 4, name: 'とてもくさい' },
+    { id: 1, name: 'くさくない' },
+    { id: 2, name: 'くさい' },
+    { id: 3, name: 'とてもくさい' },
   ]
 
   include ActiveHash::Associations
